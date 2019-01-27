ed2d21a129ac7f48eca3db9c1a2857897a95452c
Galim Kaudinov <galimk@gmail.com>
2019-01-27 12:28:16 -0700

    implemented password component

--- src/components/propertyEditors/PasswordProperties.vue
+++ src/components/propertyEditors/PasswordProperties.vue
@@ -1,10 +1,75 @@
 <template>
-
+    <b-container>
+        <b-row>
+            <b-col class="pl-0">
+                <label>Min Length</label>
+                <b-form-input type="number" v-model="$v.minLength.$model"
+                              :class="inputStatus($v.minLength)"></b-form-input>
+                <b-form-invalid-feedback>
+                    <span v-if="!$v.minLength.required">Min Length is required</span>
+                    <span v-if="!$v.minLength.greaterThanZero">Must be greater then 0</span>
+                </b-form-invalid-feedback>
+            </b-col>
+        </b-row>
+        <b-row class="mb-3 pt-3">
+            <b-form-checkbox :id="`${field.id}-special-chars`" unchecked-value="0" value="1" v-model="includeSpecialChars">
+                Must contain special characters.
+            </b-form-checkbox>
+            <b-form-checkbox :id="`${field.id}-numbers`" unchecked-value="0" value="1" v-model="includeNumbers">
+                Must contain numbers.
+            </b-form-checkbox>
+        </b-row>
+    </b-container>
 </template>
 
 <script>
+    import {required} from 'vuelidate/lib/validators'
+    import mixins from "../../mixins";
+
     export default {
-        name: "PasswordProperties"
+        name: "PasswordProperties",
+        props: ['field'],
+        mixins: [mixins],
+        data() {
+            return {
+                includeSpecialChars: this.field.includeSpecialChars ? "1" : "0",
+                includeNumbers: this.field.includeNumbers ? "1" : "0",
+                minLength: this.field.minLength || 5
+            }
+        },
+        validations: {
+            minLength: {
+                required,
+                greaterThanZero: (value) => !value || parseInt(value) > 0
+            }
+        },
+        mounted() {
+            this.$emit('validated', this.$v.$invalid);
+        },
+        // this can be refactored, to many watches just to propagate properties back
+        watch: {
+            includeSpecialChars() {
+                this.emitChanges();
+            },
+            includeNumbers() {
+                this.emitChanges();
+            },
+            minLength() {
+                this.emitChanges();
+            },
+            '$v.$invalid'() {
+                this.$emit('validated', this.$v.$invalid)
+            },
+        },
+        methods: {
+            emitChanges() {
+                this.$emit('updated', {
+                    includeSpecialChars: this.includeSpecialChars,
+                    includeNumbers: this.includeNumbers,
+                    minLength: this.minLength
+                });
+            }
+        }
     }
 </script>
 
